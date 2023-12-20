9eca76af904ff2496d6f9b198b8986c76d1105f4
9TalesGithub <112519255+9TalesGithub@users.noreply.github.com>
2023-12-19 22:00:57 -0600

    Made the code support multiple functions with same name

--- c3script.java
+++ c3script.java
@@ -17,18 +17,19 @@ import ghidra.program.model.symbol.*;
 import ghidra.program.model.scalar.*;
 import ghidra.program.model.listing.*;
 import ghidra.program.model.address.*;
+import java.util.ArrayList;
 
 public class c3script extends GhidraScript {
 
     public void run() throws Exception {
 	Program program = currentProgram;
-	FunctionManager functionManager = program.getFunctionManager();
 
         //================== Get the function ===================
+	FunctionManager functionManager = program.getFunctionManager();
         boolean functionFound = false;
 	boolean incorrectFunctionEntered = false;
 	boolean functionHasVariables = true;
-	Function desiredFunction = getFirstFunction();
+	ArrayList<Function> desiredFunctions = new ArrayList<Function>();
 	String desiredFunctionString = "";
 
         while (!functionFound) {
@@ -43,8 +44,7 @@ public class c3script extends GhidraScript {
                 desiredFunctionString = askString("Function Name", "What function are you checking the variables from?");
 	    }
 
-	    //Reinitializing some error message related variables
-	    incorrectFunctionEntered = false;
+	    //Reinitializing functionHasVariables so the script doesn't give the wrong message when a function doesn't exist if the previous function didn't have variables
 	    functionHasVariables = true;
 
             //Determine if the function exists
@@ -57,12 +57,11 @@ public class c3script extends GhidraScript {
 		    if (currentFunction.getLocalVariables().length == 0) {
 			//It exists, but doesn't have any variables
 			functionHasVariables = false;
-			break;
 		    }
 		    else {
-                        desiredFunction = currentFunction;
+			//A valid function of that name was found
+                        desiredFunctions.add(currentFunction);
                         functionFound = true;
-                        break;
 		    }
                 }
             }
@@ -77,27 +76,29 @@ public class c3script extends GhidraScript {
         //================== Get the variable ===================
         boolean variableFound = false;
 	boolean incorrectVariableEntered = false;
-	Variable desiredVariable = desiredFunction.getLocalVariables()[0];
         String desiredVariableString = "";
+	ArrayList<Variable> desiredVariables = new ArrayList<Variable>();
 
         while (!variableFound) {
             //Get the variable name
 	    if (incorrectVariableEntered) {
-		desiredVariableString = askString("Variable Name", "There is no variable by the name \"" + desiredVariableString + "\" in the function \"" + desiredFunctionString + "\". What variable do you want to check for modifications of?");
+		desiredVariableString = askString("Variable Name", "There is no variable by the name \"" + desiredVariableString + "\" in any function with the name \"" + desiredFunctionString + "\". What variable do you want to check for modifications of?");
 	    }
 	    else {
                 desiredVariableString = askString("Variable Name", "What variable do you want to check for modifications of in the function \"" + desiredFunctionString + "\"?");
 	    }
 
             //Determine if the variable exists
-            Variable[] functionVariables = desiredFunction.getLocalVariables();
-            for (Variable vari : functionVariables) {
-                if (desiredVariableString.equals(vari.getName())) {
-                    variableFound = true;
-		    desiredVariable = vari;
-                    break;
+	    for (int i = 0; i < desiredFunctions.size(); i++) {
+                Variable[] functionVariables = desiredFunctions.get(i).getLocalVariables();
+                for (Variable vari : functionVariables) {
+                    if (desiredVariableString.equals(vari.getName())) {
+                        variableFound = true;
+		        desiredVariables.add(vari);
+                        break;
+                    }
                 }
-            }
+	    }
 
             //Add an error message if the variable does not exist
             if (!variableFound) {
@@ -106,31 +107,41 @@ public class c3script extends GhidraScript {
         }
         
         //================== Find all write locations to that variable ==================
-	println("The following write locations for variable \"" + desiredVariableString + "\" in function \"" + desiredFunctionString + "\" were found:");
-	boolean writeLocationsFound = false;   
+	//Printing how many functions had that variable if there wasn't just one
+	if (desiredVariables.size() != 1) {
+	    println("The variable \"" + desiredVariableString + "\" exists in " + desiredVariables.size() + " functions with the name \"" + desiredFunctionString + "\". Here are the results for each of them.");
+	}
+
 	ReferenceManager referenceManager = program.getReferenceManager();
+	
+	//Iterating through each function
+	for (Variable desiredVariable : desiredVariables) {
+	    println("The following write locations for variable \"" + desiredVariableString + "\" in function \"" + desiredFunctionString + "\" at address " + desiredVariable.getFunction().getEntryPoint().toString() + " were found:");
+	    boolean writeLocationsFound = false;   
+	
 
-	//Get the references to the variable         
-	Reference[] desiredVariableReferences = referenceManager.getReferencesTo(desiredVariable);      
+	    //Get the references to the variable         
+	    Reference[] desiredVariableReferences = referenceManager.getReferencesTo(desiredVariable);      
 	
 	
-	//Iterate over the references and determine if they are writes to the variable        
-	for (Reference reference : desiredVariableReferences) {
-	    if (reference.getReferenceType().isWrite()) {             
-	        // Get the instruction that writes to the variable             
-	        Instruction instruction = getInstructionAt(reference.getFromAddress());              
+	    //Iterate over the references and determine if they are writes to the variable        
+	    for (Reference reference : desiredVariableReferences) {
+	        if (reference.getReferenceType().isWrite()) {             
+	            // Get the instruction that writes to the variable             
+	            Instruction instruction = getInstructionAt(reference.getFromAddress());              
 
-	        // Print the address where the variable is written             
-	        println("Address " + instruction.getAddress());  
+	            // Print the address where the variable is written             
+	            println("Address " + instruction.getAddress());  
 
-	        writeLocationsFound = true; 
+	            writeLocationsFound = true; 
+	        }      
 	    }      
-	}      
 	  
 
 
-	if (!writeLocationsFound) {
-	    println("No write locations found.");
+	    if (!writeLocationsFound) {
+	        println("No write locations found.");
+	    }
 	}
 
 	popup("Results are shown in the console.");
